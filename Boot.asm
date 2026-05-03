e4afc9b82033b7bc632cbc388bb936371d9d8d8a
Wraith <235001174+star271-cmd@users.noreply.github.com>
2026-05-03 14:12:08 -0300

    Refactor Boot.asm for improved readability and structure

--- Boot.asm
+++ Boot.asm
@@ -2,38 +2,30 @@
 [ORG 0x7c00]
 
 init:
-   mov ax, 0
+   xor ax, ax
    mov ds, ax
    mov ss, ax
    mov es, ax
-
    mov sp, 0x7c00
-
-   mov si, 0
 
    mov bx, 0
 
-   call print_loop
+   mov ah, 0x02
+   mov al, 1
+   mov ch, 0
+   mov cl, 2
+   mov dh, 0
 
-print_loop:
-   mov cl, [msg+bx]
+   mov bx, 0x7e00
 
-   call print
+   int 0x13
 
-   inc bx
+   jc disk_error
 
-   cmp cl, 0
-   jnz print_loop
+   jmp 0x000:0x7e00
 
-print:
-   mov ah, 0x0e
-   mov al, cl
-   int 0x10
-   ret
-HALT:
-   jmp halt
+disk_error:
+   jmp $
 
-msg db "Loading...", 0
-
-times 510 - ($ - $$) db 0
+times 510-($-$$) db 0
 dw 0xAA55
