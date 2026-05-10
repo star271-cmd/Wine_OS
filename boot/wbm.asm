f54c77e476b9acd0c343894c83100a46909e0de8
Vinicius <235001174+star271-cmd@users.noreply.github.com>
2026-05-10 16:33:20 -0300

    Atualizar o wbm.asm

--- boot/wbm.asm
+++ boot/wbm.asm
@@ -1,21 +1,24 @@
 [BITS 16]
 [ORG 0x7c00]
 
-xor ax, ax
-mov es, ax
-mov ss, ax
-mov ds, ax
-mov sp, 0x7c00
-mov bx 0x7e00
+init:
+   xor ax, ax
+   mov es, ax
+   mov ss, ax
+   mov ds, ax
+   mov sp, 0x7c00
+   mov bx 0x7e00
 
 print:
-   int 0x10
    mov ah, 0x0e
-   
-mov ah, 0x02
-mov al, 1
-mov ch, 0
-mov cl, 2
-mov dh, 0
+   int 0x10
+
+disk_error:
+   mov al, 'an error was been ocurred when tried to boot'
+   call print
+   hlt
+
+jc disk_error
+
 
 int 0x13
