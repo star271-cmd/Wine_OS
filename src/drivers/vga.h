05b21fa2be6b94528fbc87fdf02f259ac6be4d86
Vinicius <235001174+star271-cmd@users.noreply.github.com>
2026-05-11 19:39:32 -0300

    Atualizar o vga.h

--- src/drivers/vga.h
+++ src/drivers/vga.h
@@ -1,9 +1,12 @@
 #ifndef VGA_H
 #define VGA_H
+#define VGA_ADDRESS 0xb8000
+#define COLOR 0x07
 
 void setcolor(color, target);
-void printl(text);
-void draw(shape, local);
+void printl(const char *str);
+void drawsquare(int x, int y, int width, int height, unsigned char color, int fill);
+void drawcircle(int xc, int yc, int r, unsigned char color);
 
 #endif
 return 0;
