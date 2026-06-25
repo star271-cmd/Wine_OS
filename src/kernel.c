364143f911fd097cd0938eebc20d871765b3421b
Vinicius <235001174+star271-cmd@users.noreply.github.com>
2026-06-25 09:30:53 -0300

    Atualizar o kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -3,6 +3,10 @@
 
 int kmain(){
    pruntl("BATI foundation(C)2026-2027");
+   try:
+      read(0, 1, 2, 1);
+   exept:
+      printl("Can't read the hard disk - 0x0001")
    printl("Welcome to Wine");
    printl("C:/Computer/home/user/admin>");
    return 0;
