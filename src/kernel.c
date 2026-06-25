be3c5ad68d8309299347c0687c257dd27594419b
Vinicius <235001174+star271-cmd@users.noreply.github.com>
2026-06-25 09:04:36 -0300

    Atualizar o kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -2,7 +2,8 @@
 #include audio.h
 
 int kmain(){
-   pruntl("ANVB foundation(C)2026-2027");
+   pruntl("BATI foundation(C)2026-2027");
    printl("Welcome to Wine");
+   printl("C:/Computer/home/user/admin>");
    return 0;
 };
